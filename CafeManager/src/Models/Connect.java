027f05fef1242b0fbab1d7bc74919f275cdb332f
cuongloveyou <55919737+cuongloveyou@users.noreply.github.com>
2019-12-13 23:11:40 +0700

    Add files via upload

--- CafeManager/src/Models/Connect.java
+++ CafeManager/src/Models/Connect.java
@@ -5,10 +5,33 @@
  */
 package Models;
 
-/**
- *
- * @author Datnv
- */
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Connect {
+	private static String DB_URL = "jdbc:sqlserver://localhost:1434;"
+            + "databaseName=QuanLyQuanCaffe;"
+            + "integratedSecurity=true";
+    private static String USER_NAME = "1";
+    private static String PASSWORD = "1";    
+    private static Connection conn = null;
     
+    public static Connection getConnection() throws IOException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+    public static void close() throws SQLException {
+		conn.close();
+	}
 }
