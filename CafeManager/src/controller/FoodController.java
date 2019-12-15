5c529780cbb218b4b3bfecb645b6cfff3eec1ba9
MINHHUNGTH98 <56795302+MINHHUNGTH98@users.noreply.github.com>
2019-12-15 16:23:41 +0700

    Add files via upload

--- CafeManager/src/controller/FoodController.java
+++ CafeManager/src/controller/FoodController.java
@@ -8,6 +8,14 @@ import java.sql.Statement;
 import Models.Connect;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSetMetaData;
+import java.util.Vector;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 public class FoodController {
 
@@ -80,6 +88,58 @@ public class FoodController {
             PreparedStatement prestatement = Connect.getConnection().prepareStatement("Delete FROM Food where id=?");
             prestatement.setString(1, Id);
             prestatement.executeUpdate();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
+
+    public void loadComobox(JComboBox cbCategory) {
+        try {
+            PreparedStatement ps = Connect.getConnection().prepareStatement("Select name from FoodCategory ");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                cbCategory.addItem(rs.getString("name"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
+
+    public void loadData(DefaultTableModel tbnFood, JTable tbFood, JTextField txtIdFood, JTextField tfUnitFood, JTextField tfNameFood, JComboBox cbCategory) {
+        try {
+            int number;
+            Vector row, column;
+            column = new Vector();
+            Statement statement = Connect.getConnection().createStatement();
+            ResultSet rs = statement.executeQuery("SELECT Food.id, Food.name,food.price,FoodCategory.name FROM Food, FoodCategory WHERE Food.idCategory = FoodCategory.id;");
+            ResultSetMetaData metadata = rs.getMetaData();
+            number = metadata.getColumnCount();
+
+            for (int i = 1; i <= number; i++) {
+                column.add(metadata.getColumnName(i));
+            }
+            tbnFood.setColumnIdentifiers(column);
+
+            while (rs.next()) {
+                row = new Vector();
+                for (int i = 1; i <= number; i++) {
+                    row.addElement(rs.getString(i));
+                }
+                tbnFood.addRow(row);
+                tbFood.setModel(tbnFood);
+            }
+
+            tbFood.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+                @Override
+                public void valueChanged(ListSelectionEvent e) {
+                    if (tbFood.getSelectedRow() >= 0) {
+                        txtIdFood.setText(tbFood.getValueAt(tbFood.getSelectedRow(), 0) + "");
+                        tfNameFood.setText(tbFood.getValueAt(tbFood.getSelectedRow(), 1) + "");
+                        tfUnitFood.setText(tbFood.getValueAt(tbFood.getSelectedRow(), 2) + "");
+                        cbCategory.setSelectedItem(tbFood.getModel().getValueAt(tbFood.getSelectedRow(), 3) + "");
+                    }
+                }
+            });
         } catch (Exception ex) {
             System.out.println(ex.toString());
         }
